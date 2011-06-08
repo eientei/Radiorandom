d2a2838d29b21601991933955560dfd5b3107f9e
Alexander Tumin <itakingiteasy@gmail.com>
2011-06-08 16:26:48 +0400

    Some kind of milestone

--- src/apps/server.cpp
+++ src/apps/server.cpp
@@ -3,47 +3,86 @@
 #include <cppcms/http_response.h>
 #include <cppcms/url_dispatcher.h>
 #include <cppcms/applications_pool.h>
+#include <cppcms/json.h>
+
+#include <cppdb/frontend.h>
+
 #include <iostream>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include "master.h"
 
-
 class radio_random : public cppcms::application {
+	private:
+		void set_menu(master::menu &c) {
+			c.menu_links_left.push_back(menu_item("[Index]","/"));
+			c.menu_links_left.push_back(menu_item("[Playlist]","/playlist"));
+			c.menu_links_left.push_back(menu_item("[Users]","/users"));
+			c.menu_links_right.push_back(menu_item("[Register]","/users/new"));
+			
+		}
 	public:
 		radio_random(cppcms::service &srv) : 
 			cppcms::application(srv) 
 		{
-			dispatcher().assign("/",&radio_random::index,this,1);
+			dispatcher().assign("/",&radio_random::index,this);
+			dispatcher().assign("/setup",&radio_random::setup,this);
 		}
-		void index(std::string url) {
+		void setup() {
+			master::setup c;
+			set_menu(c);
+			
+			c.already_setted_up = false;
+			c.submut_res = false;
+			
+			std::string setuplock = settings().get<std::string>("setup.lockfile");
+			
+			struct stat filestatus;
+			stat(setuplock.c_str(), &filestatus);
+			if (S_ISREG(filestatus.st_mode)) {
+				c.already_setted_up = true;
+				render("radiorandom_setup",c);
+				return;
+			}
+			if(request().request_method()=="POST") {
+				c.setupform.load(context());
+				c.submut_res = c.setupform.submit.value();
+				c.setupform.clear();
+				FILE *fp = fopen(setuplock.c_str(),"w");
+				fclose(fp);
+			}
+			render("radiorandom_setup",c);
+		}
+		void index() {
 			master::index c;
-			menu_item item;
-
-			item.name="[Index]";
-			item.href="/";
-			c.menu_current = item;
-			c.menu_links_left.push_back(item);
-
-			item.name="[Playlist]";
-			item.href="/playlist";
-			c.menu_links_left.push_back(item);
 			
-			item.name="[Users]";
-			item.href="/users";
-			c.menu_links_left.push_back(item);
+			set_menu(c);
+
+			c.menu_current = menu_item("[Index]","/");
 			
-			item.name="[Register]";
-			item.href="/users/new";
-			c.menu_links_right.push_back(item);
+			c.files = 0;
+			c.filessize = 0;
+			
+			try {
+				cppdb::session sql("sqlite3:db=db/files.db");
+				
+			} catch (std::exception const &e) {
+				std::cerr << e.what() << std::endl;
+			}
 			
 			render("radiorandom_index",c);
+		}
+		void main(std::string url) {
+			dispatcher().dispatch(url);
 		}
 };
 
 
 int main(int argc, char **argv) {
-//	printf("Server fired up\n");
+	printf("Server fired up\n");
 	try {
 		cppcms::service srv(argc,argv);
 		srv.applications_pool().mount(cppcms::applications_factory<radio_random>());
--- src/templates/html/master.h
+++ src/templates/html/master.h
@@ -2,12 +2,22 @@
 #define MASTER_H
 
 #include <cppcms/view.h>
+#include <cppcms/form.h>
 #include <vector> 
 #include <string> 
 
-struct menu_item {
-	std::string name;
-	std::string href;
+class menu_item {
+	public:
+		std::string name;
+		std::string href;
+	public:
+		menu_item() {
+			name = href = "";
+		}
+		menu_item(std::string name, std::string href) {
+			this->name = name;
+			this->href = href;
+		}
 };
 typedef std::vector<menu_item> links_set;
 
@@ -20,7 +30,48 @@ namespace master  {
     struct master :public menu {
     };
     struct index :public master {
+		size_t files;
+		size_t filessize;
+		std::string format_filessize(size_t size) {
+			char buf[1024];
+			float fsize = size;
+			std::string suffix("B");
+			if (size / 1024 > 0) {
+				suffix = "KB";
+				size /= 1024;
+				fsize /= 1024;
+			}
+			if (size / 1024 > 0) {
+				suffix = "MB";
+				size /= 1024;
+				fsize /= 1024;
+			}
+			if (size / 1024 > 0) {
+				suffix = "GB";
+				size /= 1024;
+				fsize /= 1024;
+			}
+			if (size / 1024 > 0) {
+				suffix = "TB";
+				size /= 1024;
+				fsize /= 1024;
+			}
+			snprintf(buf,1024,"%.2f",fsize);
+			return buf + std::string(" ") + suffix;
+		}
 	};
+	struct setup_form : public cppcms::form {
+		cppcms::widgets::submit submit;
+		setup_form() {
+			submit.value("Setup");
+			*this + submit;
+		}
+	};
+    struct setup :public master {
+		bool already_setted_up;
+		bool submut_res;
+		setup_form setupform;
+    };
 
 }  
 
